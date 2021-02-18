9af8c3332a222af7c244fe0d6eb9827497e970a2
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-02-18 20:46:50 +0200

    Refactor SlideContainer disable fullscreen

--- src/components/Slider/SlideContainer/index.jsx
+++ src/components/Slider/SlideContainer/index.jsx
@@ -4,40 +4,78 @@ import cx from 'classnames';
 import SliderControls from '../SliderControls';
 import PropTypes from 'prop-types';
 
-class SlideContainer extends Component {  
+class SlideContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isImageFullscreen: false,
-    }
+    };
   }
 
   handleKeyPress = (event) => {
     console.log(event);
-  }
-    
-  toggleFullScreen =() => {
-    const {isImageFullscreen} = this.state;
-    // console.log(this);
-    isImageFullscreen?document.exitFullscreen()
-    : document.querySelector(`.${styles.image}`).requestFullscreen();
+  };
 
-    document.onfullscreenchange = this.setState((state, props) => {return {isImageFullscreen: !state.isImageFullscreen}});
-    // this.setState((state, props) => {return {isImageFullscreen: !state.isImageFullscreen}})
+  exitFullScreenHandler = (e) => {
+    // console.log(!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement);
+  //   if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+  //     document.exitFullscreen();
+  //     document.removeEventListener('fullscreenchange', this.exitFullScreenHandler);
+  //     document.removeEventListener('webkitfullscreenchange', this.exitFullScreenHandler);
+  //     document.removeEventListener('mozfullscreenchange', this.exitFullScreenHandler);
+  //     document.removeEventListener('MSFullscreenChange', this.exitFullScreenHandler);
+  //     this.setState((state, props) => {
+  //       return { isImageFullscreen: !state.isImageFullscreen };
+  //     });
+  // }
   }
+
+  toggleFullScreen = () => {
+    // const { isImageFullscreen } = this.state;
+    // if (!isImageFullscreen && !document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+    //   document.querySelector(`.${styles.image}`).requestFullscreen();
+    //   document.addEventListener('fullscreenchange', this.exitFullScreenHandler);
+    //   document.addEventListener('webkitfullscreenchange', this.exitFullScreenHandler);
+    //   document.addEventListener('mozfullscreenchange', this.exitFullScreenHandler);
+    //   document.addEventListener('MSFullscreenChange', this.exitFullScreenHandler);
+    //   this.setState((state, props) => {
+    //     return { isImageFullscreen: !state.isImageFullscreen };
+    //   });
+    // } else {
+    //   this.exitFullScreenHandler();
+    // }
+
+
+  };
 
   render() {
-    const {imageData,changeSlide, slideId} = this.props;
+    const { imageData, changeSlide, slideId } = this.props;
     return (
       <>
-        <SliderControls changeSlide={changeSlide}/>
-      <div className={styles.slideContainer}>
-        <button className={cx(styles.leftBtn, styles.btn)} onClick={()=> {changeSlide('-')}}>{`<`}</button>
-        <div className={styles.imageWrapper}>
-          <img tabIndex='0' onKeyPress={this.handleKeyPress} onClick={this.toggleFullScreen} className={styles.image}src={imageData[slideId].src} alt={imageData[slideId].alt} title={imageData[slideId].alt}/>
+        <SliderControls changeSlide={changeSlide} />
+        <div className={styles.slideContainer}>
+          <button
+            className={cx(styles.leftBtn, styles.btn)}
+            onClick={() => {
+              changeSlide('-');
+            }}
+          >{`<`}</button>
+          <div className={styles.imageWrapper}>
+            <img
+              onClick={this.toggleFullScreen}
+              className={cx(styles.image)}
+              src={imageData[slideId].src}
+              alt={imageData[slideId].alt}
+              title={imageData[slideId].alt}
+            />
+          </div>
+          <button
+            className={cx(styles.rightBtn, styles.btn)}
+            onClick={() => {
+              changeSlide('+');
+            }}
+          >{`>`}</button>
         </div>
-        <button className={cx(styles.rightBtn, styles.btn)} onClick={()=> {changeSlide('+')}}>{`>`}</button>
-      </div>
       </>
     );
   }
