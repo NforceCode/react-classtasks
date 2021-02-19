864e3235bec055d1bbfdb4d6378542b347e3ee85
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-02-19 20:11:15 +0200

    Switch to Slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Counter from './components/Counter';
+import FlexContainer from './components/FlexContainer';
 import Slider from './components/Slider';
 
 
@@ -8,25 +9,25 @@ const data = [
   {
     src: 'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-cosmic-reef-2400x1200.jpg?t=tn1200',
     heading: 'THE COSMIC REEF',
-    text: 'LOREM IPSUM WHERE IS BUTTON FOR TOGGLING AUTO SLIDE OFF'
+    text: '1 LOREM IPSUM WHERE IS BUTTON FOR TOGGLING AUTO SLIDE OFF'
   },
   {
     src: 'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-lagoon-nebula.jpg?t=tn1200',
     heading: 'BULLSHIT 2',
-    text: 'LOREM IPSUM WHERE IS BUTTON FOR TOGGLING AUTO SLIDE OFF'
+    text: '2 LOREM IPSUM WHERE IS BUTTON FOR TOGGLING AUTO SLIDE OFF'
   },
   {
     src: 'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-hh24.jpg?t=tn1200',
     heading: 'PLZ MAKE IT STOP',
-    text: 'LOREM IPSUM WHERE IS BUTTON FOR TOGGLING AUTO SLIDE OFF'
+    text: '3 LOREM IPSUM WHERE IS BUTTON FOR TOGGLING AUTO SLIDE OFF'
   },
 ]
 
 const App = () => {
   return (
     <div className='App'>
-      {/* <Slider data={data}/> */}
-      <Counter />
+      <Slider data={data}/>
+      {/* <Counter /> */}
     </div>
   );
 }
