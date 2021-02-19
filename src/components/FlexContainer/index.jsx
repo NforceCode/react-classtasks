4b69e3cdb89b24bf3adcfdc910fba415f8218502
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-02-19 19:55:49 +0200

    Add FlexContainer

---
+++ src/components/FlexContainer/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FlexContainer = (props) => {
+  const {children, jc, ai, direct} = props;
+  const inlineStyles = {
+    display: 'flex',
+    justifyContent: jc,
+    alignItems: ai,
+    flexDirection: direct,
+  }
+  return (
+    <div style={inlineStyles}>
+      {children}
+    </div>
+  );
+}
+
+FlexContainer.defaultProps = {
+  jc: 'flex-start',
+  ai: 'stretch',
+  direct: 'row',
+}
+
+FlexContainer.propTypes ={
+  jc: PropTypes.string,
+  ai: PropTypes.string,
+  direct: PropTypes.string,
+  children: PropTypes.arrayOf(PropTypes.element)
+}
+
+export default FlexContainer;
