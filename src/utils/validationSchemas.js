161cf4ab0dd07cbaae01db8cfeec4def975c5398
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-10 20:40:48 +0200

    Refactor chema

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
-export const TASK_SCHEMA = Yup.string().matches(/^[A-Z][\w\W]{1,}$/, 'Task must start with Uppercase letter').required();
+export const TASK_SCHEMA = Yup.string().matches(/^[\w][\w\W]{1,}$/, 'Task must start with non special symbol');
 
 export const TODO_APP_SCHEMA = Yup.object({
   text: TASK_SCHEMA,
