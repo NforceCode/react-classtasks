180033f88ad274cc3c1ada755a8f882dc81223e4
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-10 20:40:38 +0200

    Refactor styling

--- src/components/TodoApp/Task/index.jsx
+++ src/components/TodoApp/Task/index.jsx
@@ -1,16 +1,35 @@
-import { Field } from 'formik';
 import React from 'react';
+import cx from 'classnames';
 import styles from './Task.module.scss';
 
-const Task = (props) => {
-  const {task:{text, isCompleted, id}, toggleTaskCompletion, deleteTask} = props;
+const Task = props => {
+  const {
+    task: { text, isCompleted, id },
+    toggleTaskCompletion,
+    deleteTask,
+  } = props;
+  const taskStyles = cx(styles.task,{
+    [styles.completed]: isCompleted,
+  });
   return (
     <li className={styles.container}>
-      <input onChange={()=> {toggleTaskCompletion(id)}} type='checkbox' /> 
-      <p >{text} </p>
-      <input type='button' onClick={()=>{deleteTask(id)}} value='x'/>
+      <input
+        onChange={() => {
+          toggleTaskCompletion(id);
+        }}
+        type='checkbox'
+      />
+      <p className={taskStyles}>{text} </p>
+      <input
+        type='button'
+        className={styles.deleteBtn}
+        onClick={() => {
+          deleteTask(id);
+        }}
+        value='x'
+      />
     </li>
   );
-}
+};
 
 export default Task;
