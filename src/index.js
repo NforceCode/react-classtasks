2ce4b85549e86d891c3656a4eaf7a7f3b8130d85
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-10 20:40:57 +0200

    Add task solutions

--- src/index.js
+++ src/index.js
@@ -4,12 +4,51 @@ import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
+// fetch('https://randomuser.me/api/')
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+//   .catch(err => console.error('Vse propalo'));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// const giveMePrimitives = async () => {
+//   return [
+//     String('test'),
+//     Number(123),
+//     Symbol('hiThere'),
+//     Boolean(false),
+//     undefined,
+//     45n,
+//     null,
+//   ];
+// };
+
+// const giveMeResultsNow = async () => {
+//   // const data = await giveMePrimitives();
+
+//   // console.log(data);
+
+//   const result = await twoSecsToHell();
+//   console.log(result);
+// };
+// giveMeResultsNow();
+
+// async function twoSecsToHell () {
+//   const response =new Promise(res => {
+//     setTimeout(() => {
+//       res('Pog');
+//     }, 2000);
+//   });
+
+//   const data = await response;
+
+//   return data;
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
