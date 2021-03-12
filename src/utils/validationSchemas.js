c30e54abb27e1e79006a93864e2df9c5d96ec9d7
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-03-12 17:50:44 +0200

    Refactor validation schema

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
-export const TASK_SCHEMA = Yup.string().matches(/^[\w][\w\W]{1,}$/, 'Task must start with non special symbol');
+export const TASK_SCHEMA = Yup.string().matches(/^[\w][\w\W]{1,}$/, 'Task must start with non special symbol').required();
 
 export const TODO_APP_SCHEMA = Yup.object({
   text: TASK_SCHEMA,
